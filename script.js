0e18e3a67062384770dc89042dfea7b1b120081b
andreas svensson <andsveweb@gmail.com>
2024-03-02 11:10:31 +0100

    scroll to top

--- script.js
+++ script.js
@@ -65,8 +65,18 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
- 
+  let mysBtn = document 
+  .getElementById('scrollbuttonid'); 
 
+window.addEventListener('scroll', function () { 
+  if (document.body.scrollTop > 20 
+      || document.documentElement.scrollTop > 20) { 
+      mysBtn.style.display = 'block'; 
+  } else { 
+      mysBtn.style.display = 'none'; 
+  } 
+});
+  
 
 
   
