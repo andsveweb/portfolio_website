29d90044c7fc1339af8942a16068db00648f8772
andreas svensson <andsveweb@gmail.com>
2024-03-02 10:08:32 +0100

    Blure, cursor
    Added cursor light effect

--- script.js
+++ script.js
@@ -1,63 +1,6 @@
-let words = document.querySelectorAll(".word");
-words.forEach((word) => {
-    let letters = word.textContent.split("");
-    word.textContent="";
-    letters.forEach((letter) => {
-        let span = document.createElement("span");
-        span.textContent = letter;
-        span.className = "letter";
-        word.append(span);
-    });
-});
 
-let currentWordIndex = 0;
-let maxWordIndex = words.length -1;
-words[currentWordIndex].style.opacity = "1";
-
-let changeText = () => {
-    let currentWord = words[currentWordIndex];
-    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
-
-    Array.from(currentWord.children).forEach((letter,i) => {
-        setTimeout(() => {
-            letter.className = "letter out";
-        },i * 80);
-    });
-    nextWord.style.opacity="1";
-    Array.from(nextWord.children).forEach((letter,i) => {
-        letter.className = "letter behind";
-        setTimeout(() => {
-            letter.className = "letter in";
-
-        }, 340 + i *80)
-    });
-    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
-};
-
-changeText();
-setInterval(changeText, 3000)
-
-const circle = document.querySelectorAll('.circle');
-circle.forEach(elem=> {
-    var dots = elem.getAttribute("data-dots");
-    var marked = elem.getAttribute("data-percent");
-    var percent = Math.floor(dots*marked/100);
-    var points = "";
-    var rotate = 360 / dots;
-
-    for(let i = 0; i < dots ; i++) {
-        points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`
-    }
-    elem.innerHTML = points;
-
-    const pointsMarked = elem.querySelectorAll('.points');
-    for(let i = 0; i<percent ; i++) {
-        pointsMarked[i].classList.add('marked')
-    }
-})
 
 var mixer = mixitup('.portfolio-gallery');
-
 
 let menuLi = document.querySelectorAll('header ul li a');
 let section = document.querySelectorAll('section');
@@ -104,47 +47,28 @@ const observer = new IntersectionObserver((entries) => {
 const scrollScale = document.querySelectorAll(".scroll-scale");
 scrollScale.forEach((el) =>observer.observe(el));
 
-const scrollBottom = document.querySelectorAll(".scroll-bottom");
-scrollBottom.forEach((el) =>observer.observe(el));
 
-const scrollTop = document.querySelectorAll(".scroll-top");
-scrollTop.forEach((el) =>observer.observe(el));
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const readMoreLinks = document.querySelectorAll(".read-more");
-    const modalOverlay = document.querySelector(".modal-overlay");
-    const modalPopup = document.querySelector(".modal-popup");
-    const modalClose = document.querySelector(".modal-close");
-    const modalInnerContent = document.querySelector(".modal-inner-content");
-
-    readMoreLinks.forEach(function (link) {
-        link.addEventListener("click", function (event) {
-            event.preventDefault();
-            const serviceBox = link.closest(".service-box");
-            const hiddenContent = serviceBox.querySelector(".hidden-content");
-            const serviceTitle = serviceBox.querySelector("h3").textContent;
-            const serviceText = hiddenContent.innerHTML;
-
-            modalInnerContent.innerHTML = `
-                <h3>${serviceTitle}</h3>
-                <div>${serviceText}</div>
-            `;
-
-            modalOverlay.style.display = "block";
-            modalPopup.style.display = "block";
-        });
+document.addEventListener('DOMContentLoaded', function() {
+    const cursorLight = document.getElementById('cursorLight');
+  
+    document.addEventListener('mousemove', (e) => {
+      // Show the light effect when the mouse moves
+      cursorLight.style.display = 'block';
+      // Update the position of the light effect based on the mouse coordinates
+      cursorLight.style.left = `${e.clientX}px`;
+        cursorLight.style.top = `${e.clientY}px`;
     });
+  
+    document.addEventListener('mouseout', () => {
+      // Hide the light effect when the mouse leaves the window
+      cursorLight.style.display = 'none';
+    });
+  });
+  
 
-    modalClose.addEventListener("click", closeModal);
-    modalOverlay.addEventListener("click", closeModal);
 
-    function closeModal() {
-        modalOverlay.style.display = "none";
-        modalPopup.style.display = "none";
-    }
-});
+
+
 
 
 
